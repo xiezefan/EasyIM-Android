09c6a4c259a06bad79b36a374416fe8e1447a825
xiezefan-pc <xiezefan@126.com>
2014-10-11 16:14:31 +0800

    modify android project

--- server/routes/groups.js
+++ server/routes/groups.js
@@ -1,14 +1,29 @@
 var redis = require('redis');
 var Config = require('../config/server-config');
 var Util = require('../common/unit.js');
-
+var Group = require('../models/group.js');
 
 var groups = {
     create : function(req, res, next) {
         var reqData = req.body;
         var user = req.user;
 
+        var group = new Group();
+        group.name = reqData.name;
+        group.save(function(err, group) {
+            group.addUsers(user);
+        });
+    },
+    join : function(req, res, next) {
+        var reqData =  req.body;
+        var params = req.params;
+        var user = req.user;
+        Group.findOne({where:{id:params.id}}, function(err, group) {
+            group.addUsers(user);
+        });
+
     }
+
 };
 
 function createGroup(users, callback) {
